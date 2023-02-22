300cf9e32c14336f59994f2ad2b52dd09bb82940
Marko Pavlenko <mark.pavlenko@megadevllc.com>
2023-02-22 16:01:51 +0300

    style: added nft card styles

--- frontend/src/components/GetNFT.vue
+++ frontend/src/components/GetNFT.vue
@@ -1,8 +1,11 @@
 <template>
-  <div id="layout">
-    <h1> GET YOUR NFT </h1>
-    <img src="@/assets/images/get-nft-card.png" alt="get-nft-card"/>
+  <div id="main">
+    <div id="title-block-layout">
+      <p id="get-nft-title">GET <br/>YOUR NFT</p>
+    </div>
+      <img src="@/assets/images/get-nft-card.png" alt="get-nft-card"/>
   </div>
+
 </template>
 
 <script>
@@ -13,14 +16,37 @@ export default {
 
 <style scoped>
 
-#layout{
-  background-color: blue;
+#main {
+  display: flex;
+  align-items: center;
   height: 925px;
+  background: #000000;
 }
 
-img {
-  max-width: 50%;
-  float: right;
+#main img {
+  width: 75%;
+  margin-bottom: 35px;
+}
+
+#title-block-layout{
+  flex: 1;
+  display: flex;
+  align-items: center;
+}
+
+#get-nft-title {
+  flex: 1;
+  display: flex;
+  align-items: center;
+  position: absolute;
+  margin-left: 50px;
+  font-family: 'Archivo Expanded';
+  font-style: normal;
+  font-weight: 800;
+  font-size: 120px;
+  line-height: 100px;
+  letter-spacing: -0.02em;
+  color: #FFFFFF;
 }
 
 </style>
