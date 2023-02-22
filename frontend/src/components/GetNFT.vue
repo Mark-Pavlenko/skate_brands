6c6384f4f352438737f08195416727d0fb705d14
Marko Pavlenko <mark.pavlenko@megadevllc.com>
2023-02-22 17:19:52 +0300

    style: made getNFT desktop

--- frontend/src/components/GetNFT.vue
+++ frontend/src/components/GetNFT.vue
@@ -2,8 +2,15 @@
   <div id="main">
     <div id="title-block-layout">
       <p id="get-nft-title">GET <br/>YOUR NFT</p>
+      <p id="how-to-get">HOW TO GET</p>
+      <p id="description">取得方法：期間内にHUF FLAGSHIP STORESまたは <br/>
+        HUF JAPAN OFFICIAL ONLINE STOREで商品をご購入した方（数に限りがございます）<br/>
+        配布期間：2023年XX月XX日〜XX月XX日
+      </p>
+      <p class="active-link">ONLINE STORE</p>
+      <p class="active-link">SHOP LIST</p>
     </div>
-    <div id="image-container">
+    <div id="img-layout">
       <img src="@/assets/images/get-nft-card.png" alt="get-nft-card"/>
     </div>
   </div>
@@ -20,45 +27,67 @@ export default {
 #main {
   display: flex;
   align-items: center;
-  height: 925px;
+  height: 1100px;
   background: #000000;
+  position: relative;
 }
 
 #main img {
-  /*width: 75%;*/
-  /*margin-bottom: 35px;*/
+  max-width: 70%;
+  margin-bottom: 200px;
 }
 
 #title-block-layout{
-  flex: 1;
   display: flex;
-  align-items: center;
+  flex-direction: column;
+  position: absolute;
+  margin-left: 50px;
+  color: #FFFFFF;
 }
 
 #get-nft-title {
   flex: 1;
   display: flex;
-  align-items: center;
-  position: absolute;
-  margin-left: 50px;
   font-family: 'Archivo Expanded';
   font-style: normal;
   font-weight: 800;
   font-size: 120px;
   line-height: 100px;
   letter-spacing: -0.02em;
-  color: #FFFFFF;
 }
 
-#image-container {
-  background-color: red;
-  height: 925px;
-  margin-left:207px;
+#how-to-get{
+  margin-top: 50px;
+  font-family: 'Archivo Expanded';
+  font-style: normal;
+  font-weight: 800;
+  font-size: 48px;
+  line-height: 40px;
+  letter-spacing: -0.02em;
 }
 
-#image-container img {
-  max-width:100%;
-  max-height:100%;
+#description {
+  margin-top: 5px;
+}
+
+.active-link{
+  font-family: 'Archivo Expanded';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 22px;
+  display: flex;
+  align-items: center;
+  color: #94C93D;
+}
+
+.active-link:first-child{
+  margin-top: 8px;
+}
+
+#img-layout{
+  display: flex;
+  justify-content: end;
 }
 
 </style>
