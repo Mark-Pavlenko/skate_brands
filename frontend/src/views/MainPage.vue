a350aca9e11ea2dea1834d859ba65205b1b2e77b
Marko Pavlenko <mark.pavlenko@megadevllc.com>
2023-02-27 15:33:35 +0300

    feat: connected local bench of images to the animation

--- frontend/src/views/MainPage.vue
+++ frontend/src/views/MainPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="trigger"></div>
   <div class="frame">
-    <img class="image" id="imgsequence" src="http://399.michaelkennedy.ch/bw_frames/0000.jpg" alt="background">
+    <img class="image" id="imgsequence" src="@/assets/3sec_sample/2sec_sample_1.png" alt="background">
   </div>
   <div id="main-page">
     <NFTSet/>
@@ -20,33 +20,34 @@ import WhatIsThis from "@/components/WhatIsThis.vue";
 import GetNFT from '@/components/GetNFT.vue';
 import RemioArtSession from '@/components/RemioArtSession.vue';
 import MainFooter from "@/components/Footer.vue";
-import { TimelineMax, Linear } from 'gsap';
-import { ScrollMagicPluginGsap } from "scrollmagic-plugin-gsap";
-ScrollMagicPluginGsap(ScrollMagic, TweenMax);
-
+import {Linear, TimelineMax} from 'gsap';
+import {ScrollMagicPluginGsap} from "scrollmagic-plugin-gsap";
 import $ from 'jquery';
+
+ScrollMagicPluginGsap(ScrollMagic, TweenMax);
 
 export default {
   name: "MainPage",
-  components: {FollowNow, NFTSet, WhatIsThis, GetNFT, RemioArtSession,     MainFooter,},
-  mounted(){
-    let framePath = 'https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/02-head-bob-turn/';
-    let nFrames = 130;
-    let fileType = ".jpg";
-    let pageLengt = 2000;
+  components: {FollowNow, NFTSet, WhatIsThis, GetNFT, RemioArtSession, MainFooter,},
+  data() {
+    return {
+      proxyImages: [],
+      // imagesPaths: []
+    }
+  },
+  mounted() {
+    this.importAll(require.context('../assets/3sec_sample/', true, /\.png$/));
 
-    let images = [];
+    let nFrames = 190;
+    let pageLength = 2000;
+
+    let imagesPaths = [];
     let numberSequence = [];
 
-    function pad(number, length) {
-      let str = '' + number;
-      while (str.length < length) { str = '0' + str; }
-      return str;
-    }
-
-    for (let i = 10; i < (nFrames); i++) {
-      images.push(framePath +pad(i, 4) + fileType);
-    }
+    this.proxyImages.map(el => {
+      console.log(el);
+      imagesPaths.push(el);
+    })
 
     for (let i = 0; i < (nFrames); i++) {
       numberSequence.push(i);
@@ -57,13 +58,13 @@ export default {
     let ImageSequenceTween = new TimelineMax()
         .to(obj, 0.5,
             {
-              curImg: images.length - 1,
+              curImg: imagesPaths.length - 1,
               roundProps: "curImg",
               repeat: 0,
               immediateRender: true,
               ease: Linear.easeNone,
               onUpdate: function () {
-                $("#imgsequence").attr("src", images[obj.curImg]);
+                $("#imgsequence").attr("src", imagesPaths[obj.curImg]);
                 $("#framesequencenumber").text(numberSequence[obj.curImg]);
 
               }
@@ -71,29 +72,33 @@ export default {
         )
 
 
-      let ImageSequenceController = new ScrollMagic.Controller();
+    let ImageSequenceController = new ScrollMagic.Controller();
 
-      let scene = new ScrollMagic.Scene({
-        triggerElement: ".trigger",
-        triggerHook: 0,
-        offset: -100,
-        duration: pageLengt,
-      })
-          .setTween(ImageSequenceTween)
-          // .addIndicators() // add indicators (requires plugin)
-          .addTo(ImageSequenceController);
-        console.log('scene', scene);
-  }
+    new ScrollMagic.Scene({
+      triggerElement: ".trigger",
+      triggerHook: 0,
+      offset: -100,
+      duration: pageLength,
+    })
+        .setTween(ImageSequenceTween)
+        // .addIndicators()
+        .addTo(ImageSequenceController);
+  },
+  methods: {
+    importAll(r) {
+      r.keys().forEach(key => (this.proxyImages.push(r(key))));
+    },
+  },
 };
 </script>
 
 <style scoped>
-#main-page{
+#main-page {
   position: absolute;
   width: 100%;
 }
 
-.trigger{
+.trigger {
   position: absolute;
   transform: translate(-50%, -50%);
   width: 20em;
